73e311a09ad7098326fb04a6f5de10074065234a
sbx12 <sbx018@gmail.com>
2021-06-02 18:43:30 -0400

    Added React Pages and Router

--- backend/frontend/src/components/App.js
+++ backend/frontend/src/components/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+} from "react-router-dom";
+
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
     constructor(props) {
@@ -8,7 +19,15 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1>Testing React</h1>;
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/'><p>This is the Home Page</p></Route>
+                    <Route path='/join' component={RoomJoinPage} />
+                    <Route path='/create' component={CreateRoomPage} />
+                </Switch>
+            </Router>
+        )
     }
 }
 
