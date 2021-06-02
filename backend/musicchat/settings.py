4f545c27a647d26cb2b6d460b6c4a94a72a6b0e1
sbx12 <sbx018@gmail.com>
2021-06-02 16:27:46 -0400

    Frontend React Setup

--- backend/musicchat/settings.py
+++ backend/musicchat/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'xheuiwdh29h289h892h12h9h12d9012j9d0j12d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
